d139b273f29e941b3250b1d22389325a70b67392
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-26 13:08:24 -0500

    setup for recycler

--- app/src/main/java/com/icaboalo/yana/ui/fragment/ActionPlanFragment.java
+++ app/src/main/java/com/icaboalo/yana/ui/fragment/ActionPlanFragment.java
@@ -3,12 +3,17 @@ package com.icaboalo.yana.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.icaboalo.yana.R;
+import com.icaboalo.yana.io.model.ActivityApiModel;
+import com.icaboalo.yana.ui.adapter.ActivityRecyclerAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by icaboalo on 26/05/16.
@@ -27,5 +32,12 @@ public class ActionPlanFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mActionPlanRecycler = (RecyclerView) view.findViewById(R.id.action_plan_recycler);
+    }
+
+    void setUpActivityRecycler(ArrayList<ActivityApiModel> activityList){
+        ActivityRecyclerAdapter activityRecyclerAdapter = new ActivityRecyclerAdapter(getActivity(), activityList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        mActionPlanRecycler.setAdapter(activityRecyclerAdapter);
+        mActionPlanRecycler.setLayoutManager(linearLayoutManager);
     }
 }
