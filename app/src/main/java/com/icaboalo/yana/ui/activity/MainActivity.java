6fc38b12003b3b8dee9c21ddcad04410adaaf64f
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-26 11:00:51 -0500

    added drawer for main activity

--- app/src/main/java/com/icaboalo/yana/ui/activity/MainActivity.java
+++ app/src/main/java/com/icaboalo/yana/ui/activity/MainActivity.java
@@ -1,17 +1,46 @@
 package com.icaboalo.yana.ui.activity;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.icaboalo.yana.R;
 import com.icaboalo.yana.util.VUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         VUtil.checkForToken(this);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationViewOnClick();
+    }
+
+    void navigationViewOnClick(){
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                }
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    void replaceFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
