c8eb294735b7fc5f0f7b205b3034999c457fa7a1
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-26 11:22:05 -0500

    fragment design

---
+++ app/src/main/java/com/icaboalo/yana/io/model/ActionPlan.java
@@ -0,0 +1,7 @@
+package com.icaboalo.yana.io.model;
+
+/**
+ * Created by icaboalo on 26/05/16.
+ */
+public class ActionPlan {
+}
---
+++ app/src/main/java/com/icaboalo/yana/ui/fragment/ActionPlanFragment.java
@@ -0,0 +1,7 @@
+package com.icaboalo.yana.ui.fragment;
+
+/**
+ * Created by icaboalo on 26/05/16.
+ */
+public class ActionPlanFragment {
+}
