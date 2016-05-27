770e3c57f7f1e34634759ae8fc0d42986baeb39f
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-05-27 15:33:41 -0500

    :octocat: Create ActivityModel class.

---
+++ app/src/main/java/com/icaboalo/yana/realm/ActivityModel.java
@@ -0,0 +1,12 @@
+package com.icaboalo.yana.realm;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by saul on 27/05/16.
+ */
+public class ActivityModel extends RealmObject{
+    private String activity_name;
+    private String getActivity_content;
+    private boolean activity_status;
+}
--- app/src/main/java/com/icaboalo/yana/realm/DayModel.java
+++ app/src/main/java/com/icaboalo/yana/realm/DayModel.java
@@ -1,19 +1,17 @@
 package com.icaboalo.yana.realm;
 
-
-
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
 /**
  * Created by saul on 27/05/16.
  */
-public class UserModel extends RealmObject {
+public class DayModel extends RealmObject{
     @PrimaryKey
     private long id;
-    private String user_name;
-    private String email;
-    private String phone_number;
-    private String password;
+    private String date;
+    private RealmList<ActivityModel> activities;
+
 
 }
