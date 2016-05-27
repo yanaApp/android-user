d1bcd80d460c46009bd5b92190c3e28a6a5fcc97
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-05-27 15:16:46 -0500

    :octocat: Create UserModel class.

--- app/src/main/java/com/icaboalo/yana/realm/UserModel.java
+++ app/src/main/java/com/icaboalo/yana/realm/UserModel.java
@@ -1,9 +1,19 @@
 package com.icaboalo.yana.realm;
 
 
-
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 /**
  * Created by saul on 27/05/16.
  */
-public class UserModel {}
+public class UserModel extends RealmObject {
+    @PrimaryKey
+    private long id;
+    private String user_name;
+    private String email;
+    private String phone_number;
+    private String password;
+
+}
