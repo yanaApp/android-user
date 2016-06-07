67d26127babc08d8355b81e535e6de4b0654bffe
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-06-07 12:14:27 -0500

    first tests

---
+++ app/src/main/java/com/icaboalo/yana/io/MockService.java
@@ -0,0 +1,7 @@
+package com.icaboalo.yana.io;
+
+/**
+ * Created by icaboalo on 07/06/16.
+ */
+public interface MockService {
+}
---
+++ app/src/main/java/com/icaboalo/yana/io/model/ContactMockModel.java
@@ -0,0 +1,7 @@
+package com.icaboalo.yana.io.model;
+
+/**
+ * Created by icaboalo on 07/06/16.
+ */
+public class ContactMockModel {
+}
