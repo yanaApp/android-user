7224b57896dee31a8b5e5b7b850e9820d2fd7782
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-07-31 20:32:32 -0500

    created Repository

--- app/src/main/java/com/icaboalo/yana/domain/respository/Repository.java
+++ app/src/main/java/com/icaboalo/yana/domain/respository/Repository.java
@@ -1,7 +1,34 @@
 package com.icaboalo.yana.domain.respository;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+import io.realm.RealmQuery;
+import rx.Observable;
+
 /**
- * @author icaboalo on 31/07/16.
+ * Interface that represents a Repository for getting Objects from the data layer.
  */
 public interface Repository {
+
+    Observable<List> getListDynamically(String url, Class domainClass, Class dataClass, boolean persist);
+
+    Observable<?> getObjectDynamically(String url, int id, Class domainClass, Class dataClass, boolean persist);
+
+    Observable<?> postObjectDynamically(String url, HashMap<String, Object> keyValuePairs, Class domainClass, Class dataClass,
+                                        boolean persist);
+
+    Observable<?> postObjectDynamically(String url, JSONObject keyValuePairs, Class domainClass, Class dataClass, boolean persist);
+
+    Observable<?> postListDynamically(String url, HashMap<String, Object> keyValuePairs, Class domainClass, Class dataClass,
+                                      boolean persist);
+
+    Observable<?> deleteObjectDynamically(String url, int id, Class domainClass, Class dataClass, boolean persist);
+
+    Observable<?> putObjectDynamically(String url, HashMap<String, Object> keyValuePairs, Class domainClass, Class dataClass,
+                                       boolean persist);
+
+    Observable<List> searchRealmList(RealmQuery query, Class domainClass);
 }
