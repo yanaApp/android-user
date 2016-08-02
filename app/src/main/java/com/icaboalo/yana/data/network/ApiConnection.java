9f81b053ffeaa8dda137aa266dd35986d0ae1d94
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-08-01 22:06:21 -0500

    added constants file and created api connection

--- app/src/main/java/com/icaboalo/yana/data/network/ApiConnection.java
+++ app/src/main/java/com/icaboalo/yana/data/network/ApiConnection.java
@@ -1,16 +1,28 @@
 package com.icaboalo.yana.data.network;
 
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.GsonBuilder;
+import com.icaboalo.yana.data.executor.JobExecutor;
+import com.icaboalo.yana.util.Constants;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.realm.RealmObject;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
 
 /**
  * @author icaboalo on 01/08/16.
  */
 public class ApiConnection {
 
-    private Retrofit mRetrofit;
+    private static Retrofit mRetrofit;
     private static final int TIME_OUT = 30;
 
     public static OkHttpClient provideOkHttpClient(){
@@ -21,4 +33,60 @@ public class ApiConnection {
         return builder.build();
     }
 
+    public static Retrofit createRetro2Client(){
+        return new Retrofit.Builder()
+                .baseUrl(Constants.API_BASE_URL)
+                .client(provideOkHttpClient())
+                .callbackExecutor(new JobExecutor())
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
+                    .setExclusionStrategies(new ExclusionStrategy() {
+                        @Override
+                        public boolean shouldSkipField(FieldAttributes f) {
+                            return f.getDeclaredClass().equals(RealmObject.class);
+                        }
+
+                        @Override
+                        public boolean shouldSkipClass(Class<?> clazz) {
+                            return false;
+                        }
+                    }).create()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
+
+    public static Observable<Object> dynamicGetObject(String url){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicGetObject(url);
+    }
+
+    public static Observable<List> dynamicGetList(String url){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicGetList(url);
+    }
+
+    public static Observable<Object> dynamicPostObject(String url, RequestBody body){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicPostObject(url, body);
+    }
+
+    public static Observable<List> dynamicPostList(String url, RequestBody body){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicPostList(url, body);
+    }
+
+    public static Observable<Object> dynamicPutObject(String url, RequestBody body){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicPutObject(url, body);
+    }
+
+    public static Observable<Object> dynamicDeleteObject(String url, RequestBody body){
+        if (mRetrofit == null)
+            mRetrofit = createRetro2Client();
+        return mRetrofit.create(RestApi.class).dynamicDeleteObject(url, body);
+    }
 }
---
+++ app/src/main/java/com/icaboalo/yana/util/Constants.java
@@ -0,0 +1,9 @@
+package com.icaboalo.yana.util;
+
+/**
+ * @author icaboalo on 01/08/16.
+ */
+public class Constants {
+
+    public static final String API_BASE_URL = "https://yana-network.herokuapp.com/api/v1/";
+}
