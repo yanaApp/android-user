857039b61a71b7fc03ce4d3868eb8381414f9e1d
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-08-05 21:03:40 -0500

    fixes

--- app/src/main/java/com/icaboalo/yana/ui/activity/LoginActivity.java
+++ app/src/main/java/com/icaboalo/yana/ui/activity/LoginActivity.java
@@ -4,26 +4,19 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.StringDef;
-import android.support.design.widget.TextInputEditText;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.icaboalo.yana.PrefConstants;
 import com.icaboalo.yana.R;
 import com.icaboalo.yana.io.ApiClient;
 import com.icaboalo.yana.io.model.UserApiModel;
-import com.icaboalo.yana.realm.UserModel;
 
 import java.io.IOException;
 
