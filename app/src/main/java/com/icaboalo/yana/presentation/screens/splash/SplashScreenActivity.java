d328e96a33c4a36360bbea2b19b298d84bb46c7a
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-08-11 11:51:16 -0500

    validations for diferent user cases

--- app/src/main/java/com/icaboalo/yana/presentation/screens/splash/SplashScreenActivity.java
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/splash/SplashScreenActivity.java
@@ -4,10 +4,10 @@ import android.content.Intent;
 import android.view.Window;
 
 import com.icaboalo.yana.R;
-import com.icaboalo.yana.old.ui.activity.LoadingActivity;
 import com.icaboalo.yana.old.ui.activity.MainActivity;
 import com.icaboalo.yana.old.ui.activity.TutorialActivity;
 import com.icaboalo.yana.presentation.screens.BaseActivity;
+import com.icaboalo.yana.presentation.screens.action_plan.loading.LoadingActivity;
 import com.icaboalo.yana.util.PrefUtils;
 
 import java.util.Timer;
@@ -41,15 +41,15 @@ public class SplashScreenActivity extends BaseActivity {
 
     private void checkForToken(){
         if (PrefUtils.getToken(getApplicationContext()).isEmpty() || PrefUtils.getToken(getApplicationContext()).equals("TOKEN")){
-            navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), TutorialActivity.class));
+            navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), TutorialActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             finish();
         } else {
             if (!PrefUtils.isDownloadCompleted(getApplicationContext())){
-                navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), LoadingActivity.class));
+                navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), LoadingActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 finish();
             }
             else {
-                navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), MainActivity.class));
+                navigator.navigateTo(getApplicationContext(), new Intent(getApplicationContext(), MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
                 finish();
             }
         }
