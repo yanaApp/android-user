9547736272e58be583d4e335a3187ab8f9d80699
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-08-11 11:46:41 -0500

    created prefs for download complete

---
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/splash/SplashScreenActivity.java
@@ -0,0 +1,7 @@
+package com.icaboalo.yana.presentation.screens.splash;
+
+/**
+ * @author icaboalo on 11/08/16.
+ */
+public class SplashScreenActivity {
+}
--- app/src/main/java/com/icaboalo/yana/util/PrefUtils.java
+++ app/src/main/java/com/icaboalo/yana/util/PrefUtils.java
@@ -12,93 +12,51 @@ public class PrefUtils {
 
     private static final int privateMode = Context.MODE_PRIVATE;
     private static final String authFile = PrefConstants.authFile;
-    private static final String tutorialFile = PrefConstants.tutorialFile;
-    private static final String profileFile = PrefConstants.profileFile;
-    private static final String firstTimeFile = PrefConstants.firstTimeFile;
-    private static final String tourFile = PrefConstants.tourFile;
+    public static final String settings = PrefConstants.settingsFile;
 
     public static String getToken(Context context){
         SharedPreferences sharedPrefs = context.getSharedPreferences(authFile, privateMode);
         return sharedPrefs.getString(PrefConstants.tokenPref, "");
     }
 
-    public static boolean isTutorialCompleted(Context context){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(tutorialFile, privateMode);
-        return sharedPrefs.getBoolean(PrefConstants.isTutorialCompleted, false);
-    }
-
-    public static void setTutorialComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(tutorialFile, privateMode);
-        sharedPrefs.edit().putBoolean(PrefConstants.isTutorialCompleted, isComplete).apply();
-    }
-
     public static boolean isProfileComplete(Context context){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(profileFile, privateMode);
+        SharedPreferences sharedPrefs = context.getSharedPreferences(settings, privateMode);
         return sharedPrefs.getBoolean(PrefConstants.isProfileCompletedPref, false);
     }
 
     public static void setProfileComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(profileFile, privateMode);
+        SharedPreferences sharedPrefs = context.getSharedPreferences(settings, privateMode);
         sharedPrefs.edit().putBoolean(PrefConstants.isProfileCompletedPref, isComplete).apply();
     }
 
-    public static boolean isContactsFirstTime(Context context){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(firstTimeFile, privateMode);
-        return sharedPrefs.getBoolean(PrefConstants.contactPref, true);
-    }
-
-    public static void setContactsFirstTime(Context context, boolean isFirstTime){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(firstTimeFile, privateMode);
-        sharedPrefs.edit().putBoolean(PrefConstants.contactPref, isFirstTime).apply();
-    }
-
-    public static boolean isProgressFirstTime(Context context){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(firstTimeFile, privateMode);
-        return sharedPrefs.getBoolean(PrefConstants.progressPref, true);
-    }
-
-    public static void setProgressFirstTime(Context context, boolean isFirstTime){
-        SharedPreferences sharedPrefs = context.getSharedPreferences(firstTimeFile, privateMode);
-        sharedPrefs.edit().putBoolean(PrefConstants.progressPref, isFirstTime).apply();
-    }
-
     public static boolean isActionPlanTourComplete(Context context){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(settings, privateMode);
         return sharedPreferences.getBoolean(PrefConstants.isActionPlanTourComplete, false);
     }
 
     public static void setActionPlanTourComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(settings, privateMode);
         sharedPreferences.edit().putBoolean(PrefConstants.isActionPlanTourComplete, isComplete).apply();
     }
 
-    public static boolean isProfileTourComplete(Context context){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
-        return sharedPreferences.getBoolean(PrefConstants.isProfileTourComplete, false);
-    }
-
-    public static void setProfileTourComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
-        sharedPreferences.edit().putBoolean(PrefConstants.isProfileTourComplete, isComplete).apply();
-    }
-
-    public static boolean isContactsTourComplete(Context context){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
-        return sharedPreferences.getBoolean(PrefConstants.isContactsTourComplete, false);
-    }
-
-    public static void setContactsTourComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
-        sharedPreferences.edit().putBoolean(PrefConstants.isContactsTourComplete, isComplete).apply();
-    }
-
     public static boolean isProgressTourComplete(Context context){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(settings, privateMode);
         return sharedPreferences.getBoolean(PrefConstants.isProgressTourComplete, false);
     }
 
     public static void setProgressTourComplete(Context context, boolean isComplete){
-        SharedPreferences sharedPreferences = context.getSharedPreferences(tourFile, privateMode);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(settings, privateMode);
         sharedPreferences.edit().putBoolean(PrefConstants.isProgressTourComplete, isComplete).apply();
+    }
+
+    public static boolean isDownloadCompleted(Context context){
+        SharedPreferences nSharedPreferences = context.getSharedPreferences(settings, Context.MODE_PRIVATE);
+        return nSharedPreferences.getBoolean(PrefConstants.isDownloadCompletedPref, false);
+    }
+
+    public static void setDownloadCompleted(Context context, boolean completed){
+        SharedPreferences.Editor nEditor = context.getSharedPreferences(settings, Context.MODE_PRIVATE).edit();
+        nEditor.putBoolean(PrefConstants.isDownloadCompletedPref, completed);
+        nEditor.apply();
     }
 }
