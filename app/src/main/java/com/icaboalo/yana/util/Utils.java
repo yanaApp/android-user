228a8e29a3522ef6127a4222b1ad272e55d75596
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-08-13 16:51:08 -0500

    added mappers to utils

--- app/src/main/java/com/icaboalo/yana/util/Utils.java
+++ app/src/main/java/com/icaboalo/yana/util/Utils.java
@@ -6,6 +6,10 @@ import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Build;
 
+import com.icaboalo.yana.data.entities.mappers.ActionPlanEntityMapper;
+import com.icaboalo.yana.data.entities.mappers.ActivityEntityMapper;
+import com.icaboalo.yana.data.entities.mappers.CategoryEntityMapper;
+import com.icaboalo.yana.data.entities.mappers.DayEntityMapper;
 import com.icaboalo.yana.data.entities.mappers.EntityDataMapper;
 import com.icaboalo.yana.data.entities.mappers.EntityMapper;
 import com.icaboalo.yana.data.entities.mappers.UserEntityMapper;
@@ -17,18 +21,16 @@ public class Utils {
 
     public static EntityMapper getDataMapper(Class dataClass) {
         switch (dataClass.getName()) {
-//            case "com.grability.rappitendero.data.entities.CityEntity":
-//                return new CityEntityMapper();
-//            case "com.grability.rappitendero.data.entities.realm_models.ProfileRealmModel":
-//                return new ProfileEntityMapper();
-//            case "com.grability.rappitendero.data.entities.realm_models.OrderHistoryResponseRealmModel":
-//                return new OrderHistoryEntityMapper();
-//            case "com.grability.rappitendero.data.entities.realm_models.incoming.IncomingRealmModel":
-//                return new IncomingEntityMapper();
-//            case "com.grability.rappitendero.data.entities.realm_models.incoming.OrdersRealmModel":
-//                return new OrderEntityMapper();
             case "com.icaboalo.yana.data.entities.realm_models.action_plan.UserRealmModel":
                 return new UserEntityMapper();
+            case "com.icaboalo.yana.data.entities.realm_models.action_plan.ActionPlanRealmModel":
+                return new ActionPlanEntityMapper();
+            case "com.icaboalo.yana.data.entities.realm_models.action_plan.ActivityRealmModel":
+                return new ActivityEntityMapper();
+            case "com.icaboalo.yana.data.entities.realm_models.action_plan.CategoryRealmModel":
+                return new CategoryEntityMapper();
+            case "com.icaboalo.yana.data.entities.realm_models.action_plan.DayRealmModel":
+                return new DayEntityMapper();
             default:
                 return new EntityDataMapper();
         }
