0b1be9b0cbbe1e646028be81ed9880f02bc9138d
Inaki Cabo <icaboalo@gmail.com>
2016-09-28 21:20:21 -0500

    added phone permissions

--- app/src/main/java/com/icaboalo/yana/presentation/screens/main/hotline/HotlineFragment.java
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/main/hotline/HotlineFragment.java
@@ -1,11 +1,20 @@
 package com.icaboalo.yana.presentation.screens.main.hotline;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.icaboalo.yana.MyApplication;
 import com.icaboalo.yana.R;
@@ -16,7 +25,9 @@ import com.icaboalo.yana.presentation.screens.GenericDetailView;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by icaboalo on 28/09/16.
@@ -26,6 +37,8 @@ public class HotlineFragment extends BaseFragment implements GenericDetailView<O
 
     @Inject
     HotlinePresenter mHotlinePresenter;
+    @Bind(R.id.ivPhoneCall)
+    ImageView ivPhoneCall;
 
     @Nullable
     @Override
@@ -48,7 +61,6 @@ public class HotlineFragment extends BaseFragment implements GenericDetailView<O
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        checkForPermissions();
     }
 
     @Override
@@ -81,7 +93,46 @@ public class HotlineFragment extends BaseFragment implements GenericDetailView<O
         return MyApplication.getInstance().getApplicationContext();
     }
 
-    private void checkForPermissions(){
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 99:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    String uri = "tel: 52908124";
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse(uri));
+                    startActivity(intent);
+
+                } else {
+                    showError("Deberas aceptar el permiso para poder utilizar este feature.");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+        }
+    }
+
+    @OnClick(R.id.ivPhoneCall)
+     void checkForPermissions(){
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.CALL_PHONE)) {
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.CALL_PHONE}, 99);
+            }
+        } else {
+            String uri = "tel: +5219981542119";
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse(uri));
+            startActivity(intent);
+        }
     }
 }
