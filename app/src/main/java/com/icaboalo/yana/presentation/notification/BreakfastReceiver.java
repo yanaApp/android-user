4c07115e445482c26d349b0c874e37da58db10c7
Inaki Cabo <icaboalo@gmail.com>
2016-10-05 18:06:02 -0500

    created 5 essential notifications

---
+++ app/src/main/java/com/icaboalo/yana/presentation/notification/BreakfastReceiver.java
@@ -0,0 +1,10 @@
+package com.icaboalo.yana.presentation.notification;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by icaboalo on 05/10/16.
+ */
+
+public class BreakfastReceiver extends BroadcastReceiver {
+}
---
+++ app/src/main/java/com/icaboalo/yana/presentation/notification/DinnerReceiver.java
@@ -0,0 +1,8 @@
+package com.icaboalo.yana.presentation.notification;
+
+/**
+ * Created by icaboalo on 05/10/16.
+ */
+
+public class DinnerReceiver {
+}
