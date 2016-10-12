171d3cccfe41beda8d1926261075f4f517ca6a99
Inaki Cabo <icaboalo@gmail.com>
2016-10-12 18:27:19 -0500

    created view for the settings

--- app/src/main/java/com/icaboalo/yana/presentation/screens/main/settings/SettingsActivity.java
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/main/settings/SettingsActivity.java
@@ -2,11 +2,32 @@ package com.icaboalo.yana.presentation.screens.main.settings;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.widget.RelativeLayout;
 
 import com.icaboalo.yana.R;
 import com.icaboalo.yana.presentation.screens.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SettingsActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.rlFoodNotification)
+    RelativeLayout rlFoodNotification;
+    @BindView(R.id.rlDayNotification)
+    RelativeLayout rlDayNotification;
+    @BindView(R.id.rlNightNotification)
+    RelativeLayout rlNightNotification;
+    @BindView(R.id.swFoodNotification)
+    SwitchCompat swFoodNotification;
+    @BindView(R.id.swDayNotification)
+    SwitchCompat swDayNotification;
+    @BindView(R.id.swNightNotification)
+    SwitchCompat swNightNotification;
 
     @Override
     public void initialize() {
@@ -16,6 +37,8 @@ public class SettingsActivity extends BaseActivity {
     @Override
     public void setupUI() {
         setContentView(R.layout.activity_settings);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 
     public static Intent getCallingIntent(Context context) {
---
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/main/settings/SettingsPresenter.java
@@ -0,0 +1,8 @@
+package com.icaboalo.yana.presentation.screens.main.settings;
+
+/**
+ * Created by icaboalo on 12/10/16.
+ */
+
+public class SettingsPresenterg {
+}
