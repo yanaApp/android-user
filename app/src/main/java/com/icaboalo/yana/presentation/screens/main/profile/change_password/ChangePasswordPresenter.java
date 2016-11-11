12a29b49353e4f1d2ff381aa760695bb92c6cb7d
Inaki Cabo <icaboalo@gmail.com>
2016-11-11 07:24:32 -0600

    added strings

---
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/main/profile/change_password/ChangePasswordPresenter.java
@@ -0,0 +1,10 @@
+package com.icaboalo.yana.presentation.screens.main.profile.change_password;
+
+import com.icaboalo.yana.presentation.screens.GenericPostPresenter;
+
+/**
+ * Created by icaboalo on 10/11/16.
+ */
+
+public class ChangePasswordPresenter extends GenericPostPresenter {
+}
