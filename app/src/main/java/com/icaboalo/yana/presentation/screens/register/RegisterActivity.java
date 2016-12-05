55cf482831d538259e571d52cf5a25cd6f3d0a06
Inaki Cabo <icaboalo@gmail.com>
2016-12-04 21:56:10 -0600

    added validations for register button enabled

--- app/src/main/java/com/icaboalo/yana/presentation/screens/register/RegisterActivity.java
+++ app/src/main/java/com/icaboalo/yana/presentation/screens/register/RegisterActivity.java
@@ -2,15 +2,17 @@ package com.icaboalo.yana.presentation.screens.register;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.icaboalo.yana.R;
 import com.icaboalo.yana.presentation.screens.BaseActivity;
 import com.icaboalo.yana.presentation.screens.GenericPostView;
 import com.icaboalo.yana.presentation.screens.login.LoginActivity;
-import com.icaboalo.yana.presentation.screens.main.loading.LoadingActivity;
 import com.icaboalo.yana.presentation.screens.register.view_model.RegisterViewModel;
 import com.icaboalo.yana.presentation.screens.schedule.ScheduleActivity;
 
@@ -21,6 +23,7 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 /**
  * @author icaboalo on 11/08/16.
@@ -31,12 +34,26 @@ public class RegisterActivity extends BaseActivity implements GenericPostView<Re
     RegisterPresenter mRegisterPresenter;
     @BindView(R.id.etFullName)
     EditText etFullName;
-    @BindView(R.id.etEmail)
+    @BindView(R.id.et_email)
     EditText etEmail;
-    @BindView(R.id.etPassword)
+    @BindView(R.id.tl_email)
+    TextInputLayout tlEmail;
+    @BindView(R.id.et_password)
     EditText etPassword;
-    @BindView(R.id.rlProgress)
+    @BindView(R.id.tl_password)
+    TextInputLayout tlPassword;
+    @BindView(R.id.et_confirm_password)
+    EditText etConfirmPassword;
+    @BindView(R.id.iv_password)
+    ImageView ivPassword;
+    @BindView(R.id.tl_confirm_password)
+    TextInputLayout tlConfirmPassword;
+    @BindView(R.id.iv_confirm_password)
+    ImageView ivConfirmPassword;
+    @BindView(R.id.rl_progress)
     RelativeLayout rlProgress;
+    @BindView(R.id.bt_register)
+    Button btRegister;
 //    @Bind(R.id.rlRetry)
 //    RelativeLayout rlRetry;
 
@@ -87,7 +104,57 @@ public class RegisterActivity extends BaseActivity implements GenericPostView<Re
         showToastMessage(message);
     }
 
-    @OnClick(R.id.btRegister)
+    @OnTextChanged(value = R.id.et_password, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void onPasswordTextChanged() {
+        if (etPassword.getText().length() <= 6) {
+            tlPassword.setErrorEnabled(true);
+            ivPassword.setVisibility(View.VISIBLE);
+            tlPassword.setError("La contraseña debe de ser mayor a 6 caracteres");
+        } else if (etPassword.getText().length() > 6) {
+            tlPassword.setError(null);
+            tlPassword.setErrorEnabled(false);
+            ivPassword.setVisibility(View.VISIBLE);
+        } else {
+            tlPassword.setError(null);
+            tlPassword.setErrorEnabled(false);
+            ivPassword.setVisibility(View.GONE);
+        }
+
+        if (tlPassword.getError() != null) {
+            ivPassword.setImageDrawable(getResources().getDrawable(R.drawable.indicator_input_error));
+        } else {
+            ivPassword.setImageDrawable(getResources().getDrawable(R.drawable.password_valid_20dp));
+        }
+    }
+
+    @OnTextChanged(value = R.id.et_confirm_password, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void onConfirmPasswordTextChanged() {
+        ivConfirmPassword.setVisibility(View.VISIBLE);
+
+        if (etConfirmPassword.getText().toString().contentEquals(etPassword.getText().toString())) {
+            tlConfirmPassword.setError(null);
+            tlConfirmPassword.setErrorEnabled(false);
+        }
+        else {
+            tlConfirmPassword.setErrorEnabled(true);
+            tlConfirmPassword.setError("Las contraseñas no coinciden");
+        }
+
+        if (tlConfirmPassword.getError() != null)
+            ivConfirmPassword.setImageDrawable(getResources().getDrawable(R.drawable.indicator_input_error));
+        else
+            ivConfirmPassword.setImageDrawable(getResources().getDrawable(R.drawable.password_valid_20dp));
+    }
+
+    @OnTextChanged(value = {R.id.et_password, R.id.et_confirm_password, R.id.et_email}, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    void onTextChanged() {
+        if (isEmailValid() && isPasswordValid())
+            btRegister.setEnabled(true);
+        else
+            btRegister.setEnabled(false);
+    }
+
+    @OnClick(R.id.bt_register)
     void register() {
         if (etEmail.getText().toString().isEmpty() || etFullName.getText().toString().isEmpty()
                 || etPassword.getText().toString().isEmpty())
@@ -105,12 +172,23 @@ public class RegisterActivity extends BaseActivity implements GenericPostView<Re
         }
     }
 
-    @OnClick(R.id.btLogin)
+    @OnClick(R.id.bt_login)
     void login() {
         navigator.navigateTo(getApplicationContext(), LoginActivity.getCallingIntent(getApplicationContext()));
     }
 
     public static Intent getCallingIntent(Context context) {
         return new Intent(context, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    }
+
+    private boolean isPasswordValid() {
+        return etPassword.getText().toString().length() > 6 &&
+                etPassword.getText().toString().contentEquals(etConfirmPassword.getText().toString());
+    }
+
+    private boolean isEmailValid() {
+        return etEmail.getText().toString().contains("@") && (etEmail.getText().toString().contains(".com")
+                || etEmail.getText().toString().contains(".net") || etEmail.getText().toString().contains(".mx")
+                || etEmail.getText().toString().contains(".org"));
     }
 }
