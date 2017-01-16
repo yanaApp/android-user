28c7e4c8bec4a446b0d5f5a9d21c5d4825d1f9ae
Inaki Cabo <icaboalo@gmail.com>
2017-01-15 21:30:37 -0600

    modified factories

--- app/src/main/java/com/icaboalo/yana/presentation/component/widget/ColoredSnackbar.java
+++ app/src/main/java/com/icaboalo/yana/presentation/component/widget/ColoredSnackbar.java
@@ -1,8 +1,29 @@
 package com.icaboalo.yana.presentation.component.widget;
 
+import android.support.annotation.ColorInt;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
 /**
  * Created by icaboalo on 15/01/17.
  */
 
 public class ColoredSnackbar {
+
+    private static View getSnackbarLayout(Snackbar snackbar) {
+        if (snackbar != null)
+            return snackbar.getView();
+        else
+            return null;
+    }
+
+    public static Snackbar color(Snackbar snackbar, @ColorInt int colorRes) {
+        View view = getSnackbarLayout(snackbar);
+
+        if (view != null) {
+            view.setBackgroundColor(colorRes);
+        }
+
+        return snackbar;
+    }
 }
--- app/src/main/java/com/icaboalo/yana/presentation/factories/SnackBarFactory.java
+++ app/src/main/java/com/icaboalo/yana/presentation/factories/SnackBarFactory.java
@@ -1,9 +1,50 @@
 package com.icaboalo.yana.presentation.factories;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+
+import com.icaboalo.yana.R;
+import com.icaboalo.yana.presentation.component.widget.ColoredSnackbar;
+
 /**
  * Created by icaboalo on 15/01/17.
  */
 
-public class SnackBarFactory {
+public class SnackbarFactory {
 
+    @StringDef({TYPE_ERROR})
+    public @interface SnackbarType {
+    }
+
+    public static final String TYPE_ERROR = "snackbar_error";
+    public static final String TYPE_INFO = "snackbar_info";
+
+    public static Snackbar getSnackbar(@SnackbarType String type, @NonNull View view,
+                                       @StringRes int stringId, int duration) {
+        return createSnackbar(type, Snackbar.make(view, stringId, duration), view.getContext());
+    }
+
+    public static Snackbar getSnackbar(@SnackbarType String type, @NonNull View view,
+                                       @NonNull CharSequence message, int duration) {
+        return createSnackbar(type, Snackbar.make(view, message, duration), view.getContext());
+    }
+
+    private static Snackbar createSnackbar(@SnackbarType String type, @NonNull Snackbar snackbar,
+                                           @NonNull Context context) {
+        switch (type) {
+            case TYPE_ERROR:
+                return ColoredSnackbar.color(snackbar, ContextCompat.getColor(context, R.color.error_red));
+
+            case TYPE_INFO:
+                return ColoredSnackbar.color(snackbar, ContextCompat.getColor(context, R.color.yana_green));
+
+            default:
+                return snackbar;
+        }
+    }
 }
