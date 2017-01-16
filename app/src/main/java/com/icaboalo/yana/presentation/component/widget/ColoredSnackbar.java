30966ca893af7f41494064cfa75b058df87fa7b2
Inaki Cabo <icaboalo@gmail.com>
2017-01-15 21:30:03 -0600

    added snackbar factory

---
+++ app/src/main/java/com/icaboalo/yana/presentation/component/widget/ColoredSnackbar.java
@@ -0,0 +1,8 @@
+package com.icaboalo.yana.presentation.component.widget;
+
+/**
+ * Created by icaboalo on 15/01/17.
+ */
+
+public class ColoredSnackbar {
+}
---
+++ app/src/main/java/com/icaboalo/yana/presentation/factories/SnackBarFactory.java
@@ -0,0 +1,9 @@
+package com.icaboalo.yana.presentation.factories;
+
+/**
+ * Created by icaboalo on 15/01/17.
+ */
+
+public class SnackBarFactory {
+
+}
